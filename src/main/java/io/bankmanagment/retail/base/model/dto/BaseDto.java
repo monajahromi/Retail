e0c050ee24c84bcc56c5075205ae837f18350a86
Mona Jahromi <62639083+monajahromi@users.noreply.github.com>
2023-08-09 23:02:06 +0330

    base model(entity,dto) created

---
+++ src/main/java/io/bankmanagment/retail/base/model/dto/BaseDto.java
@@ -0,0 +1,5 @@
+package io.bankmanagment.retail.base.model.dto;
+
+import java.io.Serializable;
+
+public interface BaseDto extends Serializable { }
---
+++ src/main/java/io/bankmanagment/retail/base/model/entity/BaseEntity.java
@@ -0,0 +1,30 @@
+package io.bankmanagment.retail.base.model.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.util.Date;
+
+@MappedSuperclass
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+public class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    @CreatedDate
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date created_at;
+
+    @LastModifiedDate
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date updated_at;
+}
