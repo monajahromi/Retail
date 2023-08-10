643e9ca3be1c29b5007b8a9940ec0583c233dc73
Mona Jahromi <62639083+monajahromi@users.noreply.github.com>
2023-08-11 01:57:49 +0330

    Enabled JPA auditing

--- src/main/java/io/bankmanagment/RetailApplication.java
+++ src/main/java/io/bankmanagment/RetailApplication.java
@@ -2,8 +2,10 @@ package io.bankmanagment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class RetailApplication {
 
     public static void main(String[] args) {
--- src/test/java/io/bankmanagment/RetailApplicationTests.java
+++ src/test/java/io/bankmanagment/RetailApplicationTests.java
@@ -2,10 +2,9 @@ package io.bankmanagment;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
 
 @SpringBootTest
-@EnableJpaAuditing
 class RetailApplicationTests {
 
     @Test
