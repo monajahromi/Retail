29928c736b4ae6eb4150d0927e6495ff2ed64b46
Mona Jahromi <62639083+monajahromi@users.noreply.github.com>
2023-08-15 14:28:40 +0330

    normalized AccountDto,
    removed validation constrains from AccountDto, removed CustomerDto fields, added List<TransactionDto>

--- src/main/java/io/bankmanagment/retail/account/AccountDto.java
+++ src/main/java/io/bankmanagment/retail/account/AccountDto.java
@@ -1,25 +1,20 @@
 package io.bankmanagment.retail.account;
 
 import io.bankmanagment.base.model.dto.BaseDto;
-import io.bankmanagment.retail.customer.CustomerDto;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
+import io.bankmanagment.retail.transaction.TransactionDto;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 @EqualsAndHashCode(callSuper = true)
 @Data
 public class AccountDto extends BaseDto {
 
-    @NotBlank(message = "Number cannot be blank")
     private String number;
     private BigDecimal balance;
-    @NotNull(message = "balanceFloor cannot be null")
     private BigDecimal balanceFloor;
-    @NotNull(message = "Customer cannot be blank")
-    private CustomerDto customer;
-
+    private List<TransactionDto> transactions;
 
 }
--- src/main/java/io/bankmanagment/retail/transaction/TransactionRepository.java
+++ src/main/java/io/bankmanagment/retail/transaction/TransactionRepository.java
@@ -1,6 +1,10 @@
 package io.bankmanagment.retail.transaction;
 
 import io.bankmanagment.base.repository.IBaseRepository;
+import io.bankmanagment.retail.account.AccountEntity;
+
+import java.util.List;
 
 public interface TransactionRepository extends IBaseRepository<TransactionEntity> {
+    List<TransactionEntity> findByAccount(AccountEntity accountEntity) ;
 }
