90e02c5ce40dc4ab444448ba96a8f61331b9e3f3
Mona Jahromi <62639083+monajahromi@users.noreply.github.com>
2023-08-15 15:07:18 +0330

    Refactored IBaseMapper interface method names and generic type parameter names for clarity
    entityToGetDto to entityToRespDto
    postDtoToEntity to reqDtoToEntity
    
    Additionally, I updated the generic type parameter names to better reflect their usage:
    
    GD to RespDto (Response Data Transfer Object)
    PD to ReqDto (Request Data Transfer Object)

--- src/main/java/io/bankmanagment/base/mapper/IBaseMapper.java
+++ src/main/java/io/bankmanagment/base/mapper/IBaseMapper.java
@@ -4,7 +4,7 @@ package io.bankmanagment.base.mapper;
 import io.bankmanagment.base.model.dto.BaseDto;
 import io.bankmanagment.base.model.entity.BaseEntity;
 
-public interface IBaseMapper<T extends BaseEntity, GD extends BaseDto, PD extends BaseDto> {
-    GD entityToGetDto(T entity);
-    T postDtoToEntity(PD dto);
+public interface IBaseMapper<T extends BaseEntity, RespDto extends BaseDto, ReqDto extends BaseDto> {
+    RespDto entityToRespDto(T entity);
+    T reqDtoToEntity(ReqDto dto);
 }
