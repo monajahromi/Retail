2a43078d98a50bda63243ee119f1d5d74658943e
Mona Jahromi <62639083+monajahromi@users.noreply.github.com>
2023-08-16 12:14:24 +0330

    Excluded (post, put, delete) methods from being exposed in TransactionController

--- src/main/java/io/bankmanagment/retail/transaction/TransactionController.java
+++ src/main/java/io/bankmanagment/retail/transaction/TransactionController.java
@@ -1,7 +1,11 @@
 package io.bankmanagment.retail.transaction;
 
 import io.bankmanagment.base.controller.BaseRestController;
+import io.bankmanagment.base.exception.CustomMethodNotAllowedException;
+import io.bankmanagment.base.exception.NotFoundException;
 import io.bankmanagment.base.service.IBaseService;
+import io.swagger.v3.oas.annotations.Operation;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,5 +16,25 @@ public class TransactionController extends BaseRestController<TransactionEntity,
 
     public TransactionController(IBaseService<TransactionEntity, TransactionResponseDto, TransactionRequestDto> baseService) {
         super(baseService);
+    }
+
+
+
+    @Override
+    @Operation(hidden = true)
+    public ResponseEntity<TransactionResponseDto> update(TransactionRequestDto dto) throws NotFoundException {
+        throw new CustomMethodNotAllowedException();
+    }
+
+    @Override
+    @Operation(hidden = true)
+    public ResponseEntity<TransactionResponseDto> create(TransactionRequestDto dto) {
+        throw new CustomMethodNotAllowedException();
+    }
+
+    @Override
+    @Operation(hidden = true)
+    public ResponseEntity<Void> deleteById(Long id) throws NotFoundException {
+        throw new CustomMethodNotAllowedException();
     }
 }
